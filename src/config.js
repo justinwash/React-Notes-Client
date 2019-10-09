4efd649613edc062efda7b9e397af40c0a6a257f
Justin Wash <justin.wash@pm.me>
2019-10-09 15:16:47 -0500

    setting up amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'us-east-2',
+    BUCKET: 'jkw-attachments'
+  },
+  apiGateway: {
+    REGION: 'us-east-2',
+    URL: 'https://8iyksg6w5b.execute-api.us-east-2.amazonaws.com/prod'
+  },
+  cognito: {
+    REGION: 'us-east-2',
+    USER_POOL_ID: 'us-east-2_k3I56jBNT',
+    APP_CLIENT_ID: '48qc18go6vhi45k7939eqdo0se',
+    IDENTITY_POOL_ID: 'us-east-2:fac63338-a2e7-4859-9c5b-ecaf9ccd477b'
+  }
+};
---
+++ src/containers/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NotFound.css';
+
+export default function NotFound() {
+  return (
+    <div className='NotFound'>
+      <h3>
+        Insert error message here. I wasn't going to be helpful to you anyway.
+      </h3>
+    </div>
+  );
+}
